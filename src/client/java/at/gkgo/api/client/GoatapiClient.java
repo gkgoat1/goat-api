c595f4944c7797116ca9340f1b435607daa1cf3c
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-05-10 02:37:45 -0400

    initial commit

---
+++ src/client/java/at/gkgo/api/client/GoatapiClient.java
@@ -0,0 +1,10 @@
+package at.gkgo.api.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GoatapiClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/at/gkgo/api/client/GoatapiDataGenerator.java
@@ -0,0 +1,11 @@
+package at.gkgo.api.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GoatapiDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
